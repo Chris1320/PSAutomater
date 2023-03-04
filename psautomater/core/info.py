feeea73f14063ddc23a4038c89ee52a6ced02eea
Chris1320 <chris1320is@protonmail.com>
2023-03-04 21:23:59 +0800

    [v0.1.1] Added logger and bug fixes
    - Added `loguru` logger.
    - Fixed `addImage()` method of `resources.py` loading images to memory
      if `load` is False.
    - Fixed `ImageManager()` class always scanning the
      `data/resources/icons` directory when initialized.
    
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- psautomater/core/info.py
+++ psautomater/core/info.py
@@ -22,11 +22,19 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from time import strftime
+from pathlib import Path
+
 NAME: str = "PSAutomater"
-VERSION: tuple[int, int, int] = (0, 1, 0)
+VERSION: tuple[int, int, int] = (0, 1, 1)
 TITLE: str = f"{NAME} v{'.'.join(map(str, VERSION))}"
 
 WINDOW_SIZE: dict[str, tuple[int, int]] = {  # (w, h)
     "min": (640, 540),
     "max": (1280, 1080)
 }
+
+logfile_path = Path("data", "logs", f"log_{strftime('%Y-%m-%d')}.txt")
+LOGFILE_FORMAT = "{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}"
+LOGGING_DEBUG_MODE = "DEBUG"
+LOGGING_RELEASE_MODE = "INFO"
