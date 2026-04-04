6d35ff31486d0f0ce0245bdb09ab88d81c29d93d
Chris1320 <me@chrisfromnowhere.me>
2026-04-04 11:52:56 +0800

    chore!: migrate to uv and re-organize dependencies
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- psautomater/__main__.py
+++ psautomater/__main__.py
@@ -1,29 +1,6 @@
-"""
-MIT License
-
-Copyright (c) 2023 Chris1320
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+"""Application entry point for launching the PSAutomater GUI."""
 
 import sys
-
 from loguru import logger
 from PySide6.QtWidgets import QApplication
 
@@ -32,22 +9,24 @@ from psautomater.gui import mainInterface
 
 
 def main() -> int:
-    """
-    The main function of the program.
+    """The main entry point of the program.
 
-    :returns: The error code of the program.
+    Returns:
+        int: The error code of the program.
     """
 
     logger.add(
         info.logfile_path,
         format=info.LOGFILE_FORMAT,
         backtrace=True,
-        level=info.LOGGING_DEBUG_MODE
-        if "--debug" in sys.argv
-        else info.LOGGING_RELEASE_MODE
+        level=(
+            info.LOGGING_DEBUG_MODE
+            if "--debug" in sys.argv
+            else info.LOGGING_RELEASE_MODE
+        ),
     )
     logger.info("The program has started.")
-    logger.debug("args: ({0})", ', '.join(sys.argv))
+    logger.debug("args: ({0})", ", ".join(sys.argv))
 
     app = QApplication(sys.argv)
     logger.debug("Minimum Window Size: {0}", info.WINDOW_SIZE["min"])
@@ -57,7 +36,7 @@ def main() -> int:
         app.primaryScreen().availableGeometry().width(),
         app.primaryScreen().availableGeometry().height(),
         app.primaryScreen().size().width(),
-        app.primaryScreen().size().height()
+        app.primaryScreen().size().height(),
     )
 
     widget = mainInterface.MainInterface()
