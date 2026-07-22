c827d37f59c9bb16471b0071dcd14d20d2f75997
Chris1320 <me@chrisfromnowhere.me>
2026-07-23 01:34:15 +0800

    feat!: update `InputReader` protocol
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- psautomater/interfaces/input_readers.py
+++ psautomater/interfaces/input_readers.py
@@ -2,11 +2,11 @@ from abc import abstractmethod
 from pathlib import Path
 from typing import Protocol
 
-from psautomater.models import InputData
+from psautomater.models import Row
 
 
 class InputReader(Protocol):
-    """A protocol for reading input files."""
+    """A plugin protocol for reading input files."""
 
     __filepath: Path
 
@@ -24,4 +24,25 @@ class InputReader(Protocol):
         return self.__filepath
 
     @abstractmethod
-    def read(self) -> InputData: ...
+    def get_column_names(self) -> list[str]:
+        """Returns a list of column names from the input file."""
+
+    @abstractmethod
+    def get_next_row(self) -> Row:
+        """Yields the next row of data as a Row object."""
+
+    @abstractmethod
+    def get_previous_row(self) -> Row:
+        """Returns the previous row of data from the input file."""
+
+    @abstractmethod
+    def get_row(self, idx: int) -> Row:
+        """Returns the row of data at the specified index from the input file."""
+
+    @abstractmethod
+    def has_next_row(self) -> bool:
+        """Returns True if there are more rows to read from the input file, False otherwise."""
+
+    @abstractmethod
+    def has_previous_row(self) -> bool:
+        """Returns True if there are previous rows to read from the input file, False otherwise."""
--- psautomater/models/__init__.py
+++ psautomater/models/__init__.py
@@ -1,19 +1,4 @@
-from dataclasses import dataclass
-from enum import Enum
 from pathlib import Path
 
-
-class EditingStrategy(Enum):
-    COM = "COM"
-    PSDTOOLS = "PSDTools"
-
-
-@dataclass
-class InputData:
-    """A dataclass holding the contents of the input data."""
-
-    filepath: Path
-    headers: list[str]
-    # 2D list of values from the input file.
-    # The inner lists represent individual rows.
-    values: list[list[str | int | float | Path]]
+# type definitions
+Row = dict[str, str | int | float | Path | None]
